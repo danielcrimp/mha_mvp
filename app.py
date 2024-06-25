0f15e21de0599e0c9c924641b6472b84b40d90da
Daniel Crimp <daniel.crimp@outlook.com>
2024-06-25 20:45:24 +0100

    eyyy

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 import streamlit as st
-import openai
+from openai import OpenAI
 from pydub import AudioSegment
+from dotenv import  load_dotenv
 
-
+load_dotenv()
 
 st.title("MH Service")
 
-input = st.chat_input('test')
+
+client = OpenAI()
+
+messages = st.container(height=300)
+if prompt := st.chat_input("Say something"):
+    messages.chat_message("user").write(prompt)
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system",
+             "content": "You are a therapist taking me through a mindfulness session. I may have had problems this week. Get me to talk through them and lead me to positivity."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    respon = completion.choices[0].message.content
+
+    messages.chat_message("assistant").write(f"{respon}")
+
 
 #
 # # Audio input
