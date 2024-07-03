1c8ff9ac6d03a772805a74a95c522253c8c15ab3
Daniel Crimp <daniel.crimp@outlook.com>
2024-07-03 22:23:02 +0100

    get multi-messages working

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import openai
-from pydub import AudioSegment
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
@@ -8,18 +7,33 @@ st.title("MH Service")
 
 client = openai.OpenAI()
 
-messages = st.container(height=300)
+if "disp_messages" not in st.session_state:
+    st.session_state.disp_messages = []
+if "message_list" not in st.session_state:
+    st.session_state.message_list = [
+        {"role": "system", "content": "You are a therapist taking me through a mindfulness session. I may have had problems this week. Get me to talk through them and lead me to positivity."},
+    ]
+
+for msg in st.session_state.disp_messages:
+    if msg["role"] == "user":
+        st.chat_message("user").write(msg["content"])
+    elif msg["role"] == "assistant":
+        st.chat_message("assistant").write(msg["content"])
+
 if prompt := st.chat_input("Say something"):
-    messages.chat_message("user").write(prompt)
+    st.chat_message("user").write(prompt)
+    st.session_state.disp_messages.append({"role": "user", "content": prompt})
+    st.session_state.message_list.append({"role": "user", "content": prompt})
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system",
-             "content": "You are a therapist taking me through a mindfulness session. I may have had problems this week. Get me to talk through them and lead me to positivity."},
-            {"role": "user", "content": prompt}
-        ]
+        messages=st.session_state.message_list
     )
+    
     respon = completion.choices[0].message.content
 
-    messages.chat_message("assistant").write(f"{respon}")
+    st.chat_message("assistant").write(respon)
+    st.session_state.disp_messages.append({"role": "assistant", "content": respon})
+    st.session_state.message_list.append({"role": "assistant", "content": respon})
+
+
